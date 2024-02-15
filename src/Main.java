cc0f92b3cc8e7e78aef4983e6f644136c5e81ed9
MichelecFernandes <michelecristinafr@gmail.com>
2024-02-15 15:50:29 -0300

    Finalizando o exercicio.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Valor 1: ");
+        Scanner n1 = new Scanner(System.in);
+        int numero1 = n1.nextInt();
+        Numero leitura1 = new Numero(numero1);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("Valor 2: ");
+        Scanner n2 = new Scanner(System.in);
+        int numero2 = n1.nextInt();
+        Numero leitura2 = new Numero(numero2);
+
+        System.out.println("Valor 3: ");
+        Scanner n3 = new Scanner(System.in);
+        int numero3 = n1.nextInt();
+        Numero leitura3 = new Numero(numero3);
+
+        System.out.println("Valor 4: ");
+        Scanner n4 = new Scanner(System.in);
+        int numero4 = n1.nextInt();
+        Numero leitura4 = new Numero(numero4);
+
+
+
+        ArrayList<Numero> numeros = new ArrayList<>();
+        numeros.add(leitura1);
+        numeros.add(leitura2);
+        numeros.add(leitura3);
+        numeros.add(leitura4);
+
+        int maior = numeros.get(0).getNumero();
+        int menor = numeros.get(0).getNumero();
+
+        for (int i = 1; i < numeros.size(); i++) {
+            int numeroLoop = numeros.get(i).getNumero();
+            if (numeroLoop > maior){
+                maior = numeroLoop;
+            } else if (numeroLoop < menor) {
+                menor = numeroLoop;
+            }
+
+        }
+
+        System.out.println("O menor valor é: " + menor);
+        System.out.println("O maior valor é: " + maior);
+        if(maior > 50){
+            System.out.println("Valor muito alto!");
         }
     }
 }
--- src/Numero.java
+++ src/Numero.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Numero {
+public class Numero {
+    private int numero;
+
+
+    public Numero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(numero);
+    }
 }
